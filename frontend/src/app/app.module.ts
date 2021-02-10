e086acd58a1596b9bfb38f785dadb2eb866b873c
Wallan Pereira <wallanpereira09@gmail.com>
2021-02-10 15:52:14 -0300

    Exibindo as Mensagens de Erro

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -10,15 +10,16 @@ import { MatInputModule } from "@angular/material/input";
 import { MatListModule } from "@angular/material/list";
 import { MatSidenavModule } from "@angular/material/sidenav";
 import { MatSnackBarModule } from "@angular/material/snack-bar";
-import { MatTableModule } from '@angular/material/table';
+import { MatTableModule } from "@angular/material/table";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { ProductCreateComponent } from "./components/product/product-create/product-create.component";
-import { ProductReadComponent } from './components/product/product-read/product-read.component';
-import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
+import { ProductDeleteComponent } from "./components/product/product-delete/product-delete.component";
+import { ProductReadComponent } from "./components/product/product-read/product-read.component";
+import { ProductUpdateComponent } from "./components/product/product-update/product-update.component";
 import { FooterComponent } from "./components/template/footer/footer.component";
 import { HeaderComponent } from "./components/template/header/header.component";
 import { NavComponent } from "./components/template/nav/nav.component";
@@ -42,6 +43,7 @@ registerLocaleData(localePt);
     ProductCreateComponent,
     ProductReadComponent,
     ProductUpdateComponent,
+    ProductDeleteComponent,
   ],
   imports: [
     CommonModule,
@@ -58,13 +60,13 @@ registerLocaleData(localePt);
     HttpClientModule,
     FormsModule,
     MatInputModule,
-    MatFormFieldModule
+    MatFormFieldModule,
   ],
   providers: [
     {
       provide: LOCALE_ID,
-     useValue: 'pt-BRL'
-    }
+      useValue: "pt-BRL",
+    },
   ],
   bootstrap: [AppComponent],
 })
---
+++ frontend/src/app/components/product/product-delete/product-delete.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { ProductModel } from "../product.model";
+import { ProductService } from "../product.service";
+
+@Component({
+  selector: "app-product-delete",
+  templateUrl: "./product-delete.component.html",
+  styleUrls: ["./product-delete.component.scss"],
+})
+export class ProductDeleteComponent implements OnInit {
+  product: ProductModel;
+
+  constructor(
+    private productService: ProductService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    /**
+     *  Forca conversao de valor String pra Number
+     *  Para converter para valor numerico adicionar + antes do this.route...
+     *  +this.route.snapshot.paramMap.get("id");
+     */
+    const id = this.route.snapshot.paramMap.get("id");
+    this.productService.readById(id).subscribe((product) => {
+      this.product = product;
+    });
+  }
+
+  deleteProduct(): void {
+    this.productService.delete(`${this.product.id}`).subscribe((product) => {
+      this.product = product;
+      this.router.navigate(["/products"]);
+      this.productService.showMessage("Produto exluido com sucesso!");
+    });
+  }
+
+  cancel(): void {
+    this.router.navigate(["/products"]);
+  }
+}
